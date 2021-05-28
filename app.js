c471a20ce259af9d1d50c6a777f4b1fb316cec57
Juan Urdaneta <juandurdanet@gmail.com>
2021-05-28 01:41:47 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+var items =[];
+
+app.get("/", (req, res) => {
+  
+  var options = {
+    weekday : 'long',
+    month : 'long',
+    day : 'numeric'
+  }
+  var today = new Date();
+  today.setDate(today.getDate());
+  var todayIs = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { dayOfTheWeek: todayIs, newTodo : items });
+
+});
+
+app.post("/", (req, res)=>{
+  var item = req.body.newTodo;
+  items.push(item);
+  res.redirect('/');
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
